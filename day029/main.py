cdd22ecde7f2313fbb04b967f9e295faeeb2c079
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2022-12-08 18:58:20 +0530

    day 29 complete

--- day029/main.py
+++ day029/main.py
@@ -1,11 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
 import random
-# ---------------------------- GLOBAL VARIABLES ---------------------------- #r̥
-LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import pyperclip
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+	# ---------------------------- GLOBAL VARIABLES ---------------------------- #
+	LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+	           'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+	           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	SYMBOLS = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+	password_letters = [random.choice(LETTERS) for _ in range(random.randint(8,10))]
+	password_symbols = [random.choice(LETTERS) for _ in range(random.randint(2,4))]
+	password_numbers = [random.choice(LETTERS) for _ in range(random.randint(2,4))]
+
+	password_list  = password_letters + password_symbols + password_numbers
+	random.shuffle(password_list)
+
+	password = "".join(password_list)
+
+	password_entry.delete(0, END)
+	password_entry.insert(0,password)
+	pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def password_save():
@@ -34,7 +54,7 @@ canvas.grid(row=0, column=1)
 # ---------------------------- LABELS ------------------------------- #
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
+email_label = Label(text="Email:")
 email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
@@ -46,11 +66,11 @@ website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "abcd@gmail.com")
-password_entry = Entry(width=21)
+password_entry = Entry(width=26)
 password_entry.grid(row=3, column=1)
 
 # ---------------------------- BUTTONS ------------------------------- #
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=30 , command=password_save)
 add_button.grid(row=4, column=1, columnspan=2)
