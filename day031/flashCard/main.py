9bb0d05986ec7457b08e3d3726bec06bb9a886e9
Rakshit Dabral <rakshitdabral1@gmail.com>
2023-03-28 12:16:53 +0530

    Day 31 completed

--- day031/flashCard/main.py
+++ day031/flashCard/main.py
@@ -2,46 +2,70 @@ from tkinter import *
 import pandas
 import random
 
-#data read
-data = pandas.read_csv("./data/french_words.csv")
-data_dict = data.to_dict(orient="records")
-chosen_card = {}
-def next_card():
-    current_card = random.choice(data_dict)
-    global chosen_card
-    chosen_card = current_card
-    canvas.itemconfig(canvas_title, text="French")
-    canvas.itemconfig(canvas_body,text=current_card["French"])
-    print(current_card["French"])
-
-def card_flip():
-
-    canvas.itemconfig(canvas_title, text="English")
-    canvas.itemconfig(canvas_body, text=chosen_card["English"])
-    canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-    canvas.grid(row=0, column=0, columnspan=2)
-
-# GUI
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
 
 window = Tk()
-window.title('Flashy')
+window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800 ,  height=526)
-card_front_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
-canvas_title = canvas.create_text(400, 150, text="Title", font=("Ariel",40,"italic"))
-canvas_body = canvas.create_text(400,256,text="Word",font=("Ariel",60,"bold"))
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0,column=0,columnspan=2)
+canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image,highlightthickness=0,command=next_card)
-unknown_button.grid(row=1,column=0)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image,highlightthickness=0,command=card_flip)
-known_button.grid(row=1,column=1)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
+
+
+
