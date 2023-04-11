9223a174d34f196b740bbc8491acb5fe97841597
Rakshit Dabral <rakshitdabral1@gmail.com>
2023-04-11 16:34:43 +0530

    +

--- day039_040/flight_search.py
+++ day039_040/flight_search.py
@@ -7,7 +7,10 @@ TEQUILA_API_KEY = "MqIyw1zWdLT9q-LrbFD1a_JFn97hfWnJ"
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        response = requests.get(url=TEQUILA_ENDPOINT)
-        code = "TESTING"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
